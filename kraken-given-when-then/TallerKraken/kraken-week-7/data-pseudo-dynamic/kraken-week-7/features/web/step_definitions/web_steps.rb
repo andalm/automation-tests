5eabc766bddd65b4ca98e2b9670136bfe55ab1ba
pruebas <ja.riverao1@uniandes.edu.co>
2021-05-22 05:00:06 +0200

    creation of step to consume random numeric

---
+++ kraken-given-when-then/TallerKraken/kraken-week-7/data-pseudo-dynamic/kraken-week-7/features/web/step_definitions/web_steps.rb
@@ -0,0 +1,14 @@
+if ENV["ADB_DEVICE_ARG"].nil?
+  require 'kraken-mobile/steps/web/kraken_steps'
+  require 'uri'
+  require 'net/http'
+
+  Then(/^I enter random into input field having id "([^\"]*)"$/) do | id|
+    $uri = URI('https://my.api.mockaroo.com/kraken-pool.json?key=40aa7c50&min_value=10&max_value=20')
+	$response = Net::HTTP.get_response($uri)
+        $numer_random = JSON.parse($response.body)[1]["numeric"]
+  	@driver.find_element(:id, id).send_keys($numer_random)
+  	sleep 2
+  end
+
+end
